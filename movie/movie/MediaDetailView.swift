aa8e978cd29b58dcadaae1fb0e762ac88eb1d0bc
rkdtnlzl <rkdtnlzl@naver.com>
2024-10-16 14:14:43 +0900

    미디어 디테일뷰 구현

---
+++ movie/movie/MediaDetailView.swift
@@ -0,0 +1,14 @@
+//
+//  MediaDetailView.swift
+//  movie
+//
+//  Created by 강석호 on 10/16/24.
+//
+
+import SwiftUI
+
+struct MediaDetailView: View {
+    var body: some View {
+        Text("hello world")
+    }
+}
